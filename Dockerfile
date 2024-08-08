280eee56add8f8102fde111ef9b8ec902f586823
Sylwester Szykuła <sylwesterszykula@gmail.com>
2024-08-08 17:58:38 +0200

    feat: init project

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM openjdk:21-slim
 COPY target/*.jar /srv/
-COPY src/main/resources/config/ /app/config/
 WORKDIR /srv/
 CMD java $JVM_OPTS -jar `find *.jar`
 
---
+++ build-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+IMAGE_NAME="sszykula/templates"
+IMAGE_TAG="spring-boot-template-1.0.0"
+
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+docker push $IMAGE_NAME:$IMAGE_TAG
